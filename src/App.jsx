1a1183e97b0eb5445b258b109b2164fbfa0fdfd5
BeygonK <kipkoechbeygon@gmail.com>
2024-07-18 10:35:10 +0300

    Created cards for each job

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
+import Jobs from './components/Jobs'
 const App = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
     <HomeCards/>
+    <Jobs/>
     </>
   )
 }

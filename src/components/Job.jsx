ca428f0a6a02edf3874c6dc9d215d86fe024db98
BeygonK <kipkoechbeygon@gmail.com>
2024-07-18 13:46:20 +0300

    fix

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
+import { FaMapMarker } from 'react-icons/fa';
 
 const Job = ({job}) => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  let description = job.description
+
+  if (!showDescription){
+    description = description.substring(0, 120) + '...'
+  }
   return (
     <div className="bg-white rounded-xl shadow-md relative">
                 <div className="p-4">
@@ -10,7 +19,10 @@ const Job = ({job}) => {
                   </div>
     
                   <div className="mb-5">
-                   {job.description}
+                   {description}
+                  <button onClick={() => setShowDescription((prevState) => !prevState)} className='text-indigo-600 mb-2 hover:text-indigo-600'>
+                    {showDescription ? 'less' : 'more'}
+                  </button>
                   </div>
     
                   <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
@@ -19,7 +31,7 @@ const Job = ({job}) => {
     
                   <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                      <i className="fa-solid fa-location-dot text-lg"></i>
+                      <FaMapMarker className='inline text-lg mb-1 mr-1'/>
                       {job.location}
                     </div>
                     <a

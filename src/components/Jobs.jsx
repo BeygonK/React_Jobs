064eec28d94389f624b775ac7e0e9a7ab424434a
BeygonK <kipkoechbeygon@gmail.com>
2024-07-20 12:28:08 +0300

    json server

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import jobs from '../Jobs.json'
 import Job from './Job'
-const Jobs = () => {
+import { useState, useEffect } from 'react'
+const Jobs = ({isHome=false}) => {
     /*<!--console.log(jobs)--!>*/
-    const recentJobs = jobs.slice(0, 3)
+    const [jobs, setJobs] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+      const fetchJobs = async () => {
+        const res = await fetch('http;://localhost:5000/jobs');
+        const data = res.json();
+        setJobs(data);
+        setLoading(false);
+      }
+      fetchJobs()
+    }, [])
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-black mb-6 text-center">
-          Featured Jobs
+          { isHome ? 'Featured Jobs': 'All Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {recentJobs.map((job) => (
+            {jobs.map((job) => (
                 <Job key={job.id} job={job}/>
             )) }
         </div>
--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Jobs from '../components/Jobs'
 const JobsPage = () => {
   return (
-    <div>JobsPage</div>
+    <section className='bg-blue-50 px-4 py-6'>
+      <Jobs/>
+    </section>
   )
 }
 
